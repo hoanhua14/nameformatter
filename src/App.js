9f9f809676cb89df416b79f57e2348078668a5e2
hoanhua14 <huasohoan@gmail.com>
2023-11-03 14:23:23 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [fullName, setFullName] = useState('');
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+
+  const handleFormat = function() {
+    const splitted = fullName.split(' ');
+    setFirstName(splitted[0]);
+    setLastName(splitted[1]);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>Enter Full Name Here:</div>
+      <input type='text' id='userInput' onChange={(e) => setFullName(e.target.value)}></input><br></br>
+      <button onClick={handleFormat}>Format</button>
+      <div>First name: {firstName}</div>
+      <div>Last name: {lastName}</div>
     </div>
   );
 }
